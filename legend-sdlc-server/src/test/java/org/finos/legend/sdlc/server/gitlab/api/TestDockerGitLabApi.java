2d7b2ae141ac09c99f5078c58ea426f64bc83c31
Jianglai Zhang <Jianglai.Zhang@gs.com>
2021-01-19 15:51:59 -0500

    test pipeline.

---
+++ legend-sdlc-server/src/test/java/org/finos/legend/sdlc/server/gitlab/api/TestDockerGitLabApi.java
@@ -0,0 +1,12 @@
+package org.finos.legend.sdlc.server.gitlab.api;
+
+import org.junit.Test;
+
+public class TestDockerGitLabApi
+{
+    @Test
+    public void testDockerCreateProject()
+    {
+
+    }
+}
