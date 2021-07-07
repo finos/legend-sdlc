972d96d7b3cfee38a9d38e52af7436dd0f69d5ff
Jianglai Zhang <Jianglai.Zhang@gs.com>
2021-07-07 10:05:41 -0400

    revert irrational pager change.

--- legend-sdlc-server/src/main/java/org/finos/legend/sdlc/server/gitlab/api/GitLabApiWithFileAccess.java
+++ legend-sdlc-server/src/main/java/org/finos/legend/sdlc/server/gitlab/api/GitLabApiWithFileAccess.java
@@ -573,18 +573,7 @@ abstract class GitLabApiWithFileAccess extends BaseGitLabApi
         private Commit getCurrentCommit(CommitsApi commitsApi, String referenceId, String filePath) throws GitLabApiException
         {
             Pager<Commit> pager = withRetries(() -> commitsApi.getCommits(this.projectId.getGitLabId(), referenceId, null, null, filePath, 1));
-
-            List<Commit> page;
-            int currentPage = pager.getCurrentPage();
-            if (currentPage == 1)
-            {
-                page = pager.page(currentPage);
-            }
-            else
-            {
-                page = pager.next();
-            }
-            return ((page == null) || page.isEmpty()) ? null : page.get(0);
+            List<Commit> page = pager.next();
         }
 
         @Override
