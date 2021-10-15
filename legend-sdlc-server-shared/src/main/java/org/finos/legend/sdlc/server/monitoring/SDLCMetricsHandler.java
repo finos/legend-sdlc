06138ccc9829e5863bae90164ece898d6bca2700
Kevin Knight <kevin.knight@gs.com>
2021-10-15 17:22:37 -0400

    Refactor SDLCMetricsHandler

--- legend-sdlc-server-shared/src/main/java/org/finos/legend/sdlc/server/monitoring/SDLCMetricsHandler.java
+++ legend-sdlc-server-shared/src/main/java/org/finos/legend/sdlc/server/monitoring/SDLCMetricsHandler.java
@@ -14,6 +14,7 @@
 
 package org.finos.legend.sdlc.server.monitoring;
 
+import io.prometheus.client.Collector;
 import io.prometheus.client.Counter;
 import io.prometheus.client.SimpleTimer;
 import io.prometheus.client.Summary;
@@ -22,7 +23,7 @@ import org.eclipse.collections.impl.map.mutable.ConcurrentHashMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.function.Function;
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 public class SDLCMetricsHandler
@@ -34,10 +35,23 @@ public class SDLCMetricsHandler
     private static final Summary OPERATION_REDIRECT_SUMMARY = createDurationSummary("sdlc_operations_redirected", "Duration summary for SDLC operations terminating with a redirect");
     private static final Summary OPERATION_ERROR_SUMMARY = createDurationSummary("sdlc_operations_errors", "Duration summary for SDLC operations terminating with an error");
 
-    private static final String METRIC_PREFIX = "sdlc_";
-    private static final Pattern METRIC_NAME_REPLACE = Pattern.compile("\\W++");
-    private static final ConcurrentMutableMap<String, Summary> ADDITIONAL_SUMMARIES = ConcurrentHashMap.newMap();
-    private static final ConcurrentMutableMap<String, Counter> ADDITIONAL_COUNTERS = ConcurrentHashMap.newMap();
+    private static final MetricsRegistry<Summary> ADDITIONAL_SUMMARIES = new MetricsRegistry<Summary>("duration summary")
+    {
+        @Override
+        protected Summary createNewMetric(String name, String help)
+        {
+            return createDurationSummary(name, help);
+        }
+    };
+
+    private static final MetricsRegistry<Counter> ADDITIONAL_COUNTERS = new MetricsRegistry<Counter>("counter")
+    {
+        @Override
+        protected Counter createNewMetric(String name, String help)
+        {
+            return createCounter(name, help);
+        }
+    };
 
     public static void operationStart()
     {
@@ -65,7 +79,7 @@ public class SDLCMetricsHandler
         durationSummary.observe(duration);
         if (durationMetricName != null)
         {
-            Summary summary = getOrCreateAdditionalDurationSummary(durationMetricName);
+            Summary summary = ADDITIONAL_SUMMARIES.getOrCreate(durationMetricName);
             if (summary != null)
             {
                 summary.observe(duration);
@@ -75,68 +89,10 @@ public class SDLCMetricsHandler
 
     public static void incrementCounter(String name)
     {
-        Counter counter = getOrCreateAdditionalCounter(name);
+        Counter counter = ADDITIONAL_COUNTERS.getOrCreate(name);
         if (counter != null)
         {
             counter.inc();
-        }
-    }
-
-    private static Summary getOrCreateAdditionalDurationSummary(String name)
-    {
-        return getOrCreateAdditionalMetric(name, ADDITIONAL_SUMMARIES, SDLCMetricsHandler::createAdditionalDurationSummary);
-    }
-
-    private static Counter getOrCreateAdditionalCounter(String name)
-    {
-        return getOrCreateAdditionalMetric(name, ADDITIONAL_COUNTERS, SDLCMetricsHandler::createAdditionalCounter);
-    }
-
-    private static <T> T getOrCreateAdditionalMetric(String name, ConcurrentMutableMap<String, T> map, Function<String, T> creator)
-    {
-        T metric = map.get(name);
-        if ((metric == null) && !map.containsKey(name))
-        {
-            synchronized (map)
-            {
-                metric = map.get(name);
-                if ((metric == null) && !map.containsKey(name))
-                {
-                    metric = creator.apply(name);
-                    map.put(name, metric);
-                }
-            }
-        }
-        return metric;
-    }
-
-    private static Summary createAdditionalDurationSummary(String name)
-    {
-        try
-        {
-            String metricName = generateMetricName(name);
-            String metricHelp = name + " duration metric";
-            return createDurationSummary(metricName, metricHelp);
-        }
-        catch (Exception e)
-        {
-            LOGGER.error("Error creating new summary \"{}\"", name, e);
-            return null;
-        }
-    }
-
-    private static Counter createAdditionalCounter(String name)
-    {
-        try
-        {
-            String metricName = generateMetricName(name);
-            String metricHelp = name + " counter";
-            return createCounter(metricName, metricHelp);
-        }
-        catch (Exception e)
-        {
-            LOGGER.error("Error creating new counter \"{}\"", name, e);
-            return null;
         }
     }
 
@@ -152,9 +108,47 @@ public class SDLCMetricsHandler
         return Counter.build(name, help).register();
     }
 
-    private static String generateMetricName(String name)
+    private abstract static class MetricsRegistry<T extends Collector>
     {
-        return METRIC_PREFIX + METRIC_NAME_REPLACE.matcher(name).replaceAll("_");
+        private static final String METRIC_PREFIX = "sdlc_";
+        private static final Pattern METRIC_NAME_REPLACE = Pattern.compile("\\W++");
+
+        private final ConcurrentMutableMap<String, T> metrics = ConcurrentHashMap.newMap();
+        private final String description;
+
+        protected MetricsRegistry(String description)
+        {
+            this.description = Objects.requireNonNull(description);
+        }
+
+        T getOrCreate(String name)
+        {
+            T metric = this.metrics.get(name);
+            if ((metric == null) && !this.metrics.containsKey(name))
+            {
+                synchronized (this)
+                {
+                    metric = this.metrics.get(name);
+                    if ((metric == null) && !this.metrics.containsKey(name))
+                    {
+                        try
+                        {
+                            String metricName = METRIC_PREFIX + METRIC_NAME_REPLACE.matcher(name).replaceAll("_");
+                            String metricHelp = name + " " + this.description;
+                            metric = createNewMetric(metricName, metricHelp);
+                        }
+                        catch (Exception e)
+                        {
+                            LOGGER.error("Error creating new {} \"{}\"", this.description, name, e);
+                        }
+                        // If metric creation fails, we put null in the map so that we don't keep trying and failing to create the same metric.
+                        this.metrics.put(name, metric);
+                    }
+                }
+            }
+            return metric;
+        }
+
+        protected abstract T createNewMetric(String name, String help);
     }
 }
-
