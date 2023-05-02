eea7056e1251cff85649fc45dafe9c95d286ae79
ivan-kyosev-gs <89931850+ivan-kyosev-gs@users.noreply.github.com>
2023-05-02 15:08:32 +0100

    Fixing the /authorized endpoint (#628)
    * Creating Auth Check resource
    
    * Adding binding
    
    * Adding error handling
    
    * Fixing style
    
    * Adding bypass paths to config file
    
    * Removing old authorized method
    
    * Removing deprecated mode parameter
    
    * Checking that the sessions is a GitLabSession

---
+++ legend-sdlc-server/src/main/java/org/finos/legend/sdlc/server/gitlab/resources/GitLabAuthCheckResource.java
@@ -0,0 +1,77 @@
+// Copyright 2023 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.sdlc.server.gitlab.resources;
+
+import org.finos.legend.sdlc.server.auth.Session;
+import org.finos.legend.sdlc.server.gitlab.GitLabAppInfo;
+import org.finos.legend.sdlc.server.gitlab.auth.GitLabAuthAccessException;
+import org.finos.legend.sdlc.server.gitlab.auth.GitLabAuthorizerManager;
+import org.finos.legend.sdlc.server.gitlab.auth.GitLabSession;
+import org.finos.legend.sdlc.server.gitlab.auth.GitLabUserContext;
+import org.finos.legend.sdlc.server.resources.BaseResource;
+import org.finos.legend.sdlc.server.tools.SessionUtil;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/auth")
+public class GitLabAuthCheckResource extends BaseResource
+{
+
+    private final HttpServletRequest httpRequest;
+    private final HttpServletResponse httpResponse;
+    private final GitLabAuthorizerManager authorizerManager;
+    private final GitLabAppInfo appInfo;
+
+    @Inject
+    public GitLabAuthCheckResource(HttpServletRequest httpRequest, HttpServletResponse httpResponse, GitLabAuthorizerManager authorizerManager, GitLabAppInfo appInfo)
+    {
+        super();
+        this.httpRequest = httpRequest;
+        this.httpResponse = httpResponse;
+        this.authorizerManager = authorizerManager;
+        this.appInfo = appInfo;
+    }
+
+    @GET
+    @Path("authorized")
+    @Produces(MediaType.APPLICATION_JSON)
+    public boolean isAuthorized()
+    {
+        return executeWithLogging("checking authorization", () ->
+        {
+            Session session = SessionUtil.findSession(httpRequest);
+
+            if (session instanceof GitLabSession)
+            {
+                GitLabUserContext gitLabUserContext = new GitLabUserContext(httpRequest, httpResponse, authorizerManager, appInfo);
+                try
+                {
+                    return gitLabUserContext.isUserAuthorized();
+                }
+                catch (GitLabAuthAccessException e)
+                {
+                    getLogger().error("Access exception occurred while checking authorization", e);
+                }
+            }
+            return false;
+        });
+    }
+}
---
+++ legend-sdlc-server/src/main/java/org/finos/legend/sdlc/server/tools/SessionUtil.java
@@ -0,0 +1,82 @@
+// Copyright 2023 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.sdlc.server.tools;
+
+import org.finos.legend.sdlc.server.auth.LegendSDLCWebFilter;
+import org.finos.legend.sdlc.server.auth.Session;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletRequestWrapper;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+public class SessionUtil
+{
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SessionUtil.class);
+
+    public static Session findSession(ServletRequest request)
+    {
+        Session session = findSession(request, 0);
+        if (session == null)
+        {
+            LOGGER.warn("Could not find SDLC session from request: {} (class: {})", request, request.getClass());
+        }
+        return session;
+    }
+
+    private static Session findSession(ServletRequest request, int depth)
+    {
+        Session sdlcSession = LegendSDLCWebFilter.getSessionFromServletRequestAttribute(request);
+        if (sdlcSession != null)
+        {
+            LOGGER.debug("got SDLC session from request attribute (depth {})", depth);
+            return sdlcSession;
+        }
+
+        if (request instanceof HttpServletRequest)
+        {
+            HttpServletRequest httpRequest = (HttpServletRequest) request;
+            HttpSession httpSession;
+            try
+            {
+                httpSession = httpRequest.getSession(false);
+            }
+            catch (Exception e)
+            {
+                httpSession = null;
+            }
+            if (httpSession != null)
+            {
+                sdlcSession = LegendSDLCWebFilter.getSessionFromHttpSession(httpSession);
+                if (sdlcSession != null)
+                {
+                    LOGGER.debug("got SDLC session from HTTP session (depth {})", depth);
+                    return sdlcSession;
+                }
+            }
+        }
+
+        if (request instanceof ServletRequestWrapper)
+        {
+            return findSession(((ServletRequestWrapper) request).getRequest(), depth + 1);
+        }
+
+        LOGGER.debug("Did not find session at depth {}; no more nested requests to check; request: {}; request class: {}", depth, request, request.getClass());
+        return null;
+    }
+}
