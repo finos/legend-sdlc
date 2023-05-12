f881ad789f5b9fb4d5d81d6dfe25803d5c2be2f0
Kevin Knight <kevin.knight@gs.com>
2023-05-12 16:37:21 -0400

    Add JUnit 4 test generation maven plugin

---
+++ legend-sdlc-test-generation-maven-plugin/src/test/resources/generated/java/org/finos/legend/sdlc/test/junit/junit4/execution/TestRelationalMapping.java
@@ -0,0 +1,12 @@
+package org.finos.legend.sdlc.test.junit.junit4.execution;
+
+import org.finos.legend.sdlc.test.junit.pure.v1.AbstractMappingTest;
+
+public class TestRelationalMapping extends AbstractMappingTest
+{
+    @Override
+    protected String getEntityPath()
+    {
+        return "execution::RelationalMapping";
+    }
+}
